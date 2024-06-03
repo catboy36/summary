e888cf6c348ccc05c51cf7b46643d08ba774af44
liuhaoyang <lhyhappymmall@sina.com>
2024-06-03 10:58:32 +0800

    [add]update layout css

--- src/CSS/main.js
+++ src/CSS/main.js
@@ -1,4 +1,70 @@
-import './style.css';
+// import './style.css';
 import './style/index.less';
 import javascriptLogo from './javascript.svg';
 import viteLogo from '/vite.svg';
+
+// bfc
+// document.querySelector('#app').innerHTML = `
+//   <div class="bfc">
+//     <div class="aside"></div>
+//     <div class="main"></div>
+//   </div>
+// `;
+
+// @media
+// document.querySelector('#app').innerHTML = `
+//   <div class="media">
+//     <div class="aside"></div>
+//     <div class="main"></div>
+//   </div>
+// `;
+
+// 水平垂直居中
+// document.querySelector('#app').innerHTML = `
+//   <div class="father">
+//     <div class="son"></div>
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="father1">
+//     <div class="son"></div>
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="father2">
+//     <div class="son"></div>
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="father3">
+//     <div class="son"></div>
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="father4">
+//     <div class="son"></div>
+//   </div>
+// `;
+
+// 两列布局，左侧固定，右侧自适应
+// document.querySelector('#app').innerHTML = `
+//   <div class="box">
+//     <div class="left"> </div>
+//     <div class="right"> </div>
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="box1">
+//     <div class="left"> </div>
+//     <div class="right"> </div>
+//   </div>
+// `;
+
+// 三栏布局
+document.querySelector('#app').innerHTML = `
+  <div class="wrap">
+    <div class="left"> </div>
+    <div class="right"> </div>
+    <div class="middle"> </div>
+  </div>
+`;
