4f2f69180246af4b1b842c8dfdb4d19061412b39
liuhaoyang <lhyhappymmall@sina.com>
2024-06-04 10:35:44 +0800

    [优化]属性继承,隐藏元素,文本溢出,绘制三角形,视差滚动

--- src/CSS/main.js
+++ src/CSS/main.js
@@ -61,10 +61,66 @@ import viteLogo from '/vite.svg';
 // `;
 
 // 三栏布局
+// document.querySelector('#app').innerHTML = `
+//   <div class="wrap">
+//     <div class="left"> </div>
+//     <div class="right"> </div>
+//     <div class="middle"> </div>
+//   </div>
+// `;
+
+// 属性继承
+// document.querySelector('#app').innerHTML = `
+//   <div class="wrap">
+//    <a href="www.baidu.com">123<span>456<span></a>
+//   </div>
+// `;
+
+// 隐藏元素 display:none,visibility:hidden,opcity:0,width/height:0,absoulte,clip-path
+// document.querySelector('#app').innerHTML = `
+//   <div class="hidden">
+//   </div>
+// `;
+
+// 文本溢出
+// 单行
+// document.querySelector('#app').innerHTML = `
+//   <div class="single-text">
+//     lalalallalalalalallalalallalalalallalalalalalallalalalalalallalalalalalalla
+//   </div>
+// `;
+// 多行(基于高度不推荐，基于行数)
+// document.querySelector('#app').innerHTML = `
+//   <div class="multi-text">
+//     lalalallalalalalallalalallalalalallalalalalalallalalalalalallalalalalalallalalalallalalalalallalalallalalalallalalalalalallalalalalalallalalalalalalla
+//   </div>
+// `;
+// document.querySelector('#app').innerHTML = `
+//   <div class="multi-text-2">
+//     lalalallalalalalallalalallalalalallalalalalalallalalalalalallalalalalalallalalalallalalalalallalalallalalalallalalalalalallalalalalalallalalalalalalla
+//   </div>
+// `;
+
+// 绘制三角形
+// document.querySelector('#app').innerHTML = `
+//   <div class="border">
+//   </div>
+// `;
+
+// 视差滚动（多层背景以不同速度移动，形成立体运动效果，带来出色视觉体验）---背景层>内容层>悬浮层
+// 1. background-attachment
 document.querySelector('#app').innerHTML = `
-  <div class="wrap">
-    <div class="left"> </div>
-    <div class="right"> </div>
-    <div class="middle"> </div>
-  </div>
+  <div class="attach a-text">1</div>
+  <div class="attach a-img1">2</div>
+  <div class="attach a-text">3</div>
+  <div class="attach a-img2">4</div>
+  <div class="attach a-text">5</div>
+  <div class="attach a-img3">6</div>
+  <div class="attach a-text">7</div>
 `;
+// 2. transform:translate3D
+// document.querySelector('#app').innerHTML = `
+//   <div class="attach one"></div>
+//   <div class="attach two"></div>
+//   <div class="attach three"></div>
+// `;
